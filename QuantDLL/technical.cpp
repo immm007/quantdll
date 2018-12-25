48080bf147ff6d4c6dd4389b6659c9299762084c
immm007 <immm007@163.com>
2018-12-25 15:51:34 +0800

    save

--- QuantDLL/technical.cpp
+++ QuantDLL/technical.cpp
@@ -1,22 +1,32 @@
 #include "stdafx.h"
 #include "TCalcFuncSets.h"
-#include <stdio.h>
-#include "ADL.h"
+#include <boost/python/numpy.hpp>
 
+namespace p = boost::python;
+namespace np = boost::python::numpy;
 //生成的dll及相关依赖dll请拷贝到通达信安装目录的T0002/dlls/下面,再在公式管理器进行绑定
 
 void adl(int len,float* outs,float* codes,float* dates,float* pfINc)
 {
-	static ADL adl("E:\\quant\\data\\technical\\adl.csv");
-	int code = codes[0];
-	if (!adl.valid(code))
-		return;
-	for (int i = 0; i != len; ++i)
+	//MessageBoxA(nullptr, "1", "log", MB_OK);
+	Py_SetPythonHome(L"E:\\anaconda3");
+	Py_Initialize();
+	if (!Py_IsInitialized())
 	{
-		int date = dates[i];
-		date += 19000000;
-		outs[i] = adl.value(code, date);
+		MessageBoxA(nullptr, "start python fail", "error", MB_OK);
 	}
+	//MessageBoxA(nullptr, "2", "log", MB_OK);
+	np::initialize();
+	//MessageBoxA(nullptr, "3", "log", MB_OK);
+	np::ndarray py_array = np::from_data(outs, np::dtype::get_builtin<float>(),
+		p::make_tuple(len),
+		p::make_tuple(sizeof(float)),
+		p::object());
+	//MessageBoxA(nullptr, "4", "log", MB_OK);
+	auto quant = p::import("quant");
+	//MessageBoxA(nullptr, "5", "log", MB_OK);
+	quant.attr("adl")(py_array);
+	//MessageBoxA(nullptr, "6", "log", MB_OK);
 }
 
 //加载的函数
--- TestQuantDLL/main.cpp
+++ TestQuantDLL/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
-#include "..\QuantDLL\ADL.h"
+#include <boost/python/numpy.hpp>
 
 using namespace std;
 
+namespace p = boost::python;
+namespace np = boost::python::numpy;
+
 int main()
 {
-	ADL adl("E:\\quant\\data\\technical\\adl.csv");
+	Py_SetPythonHome(L"E:\\anaconda3");
+	Py_Initialize();
+	np::initialize();
+	float* outs = new float[5];
+	np::ndarray py_array = np::from_data(outs, np::dtype::get_builtin<float>(),
+		p::make_tuple(5),
+		p::make_tuple(sizeof(float)),
+		p::object());
+	auto quant = p::import("quant");
+	quant.attr("adl")(py_array);
+	for (int i = 0; i != 5; ++i)
+	{
+		cout << outs[i];
+	}
 	cin.get();
 }
